a5c94dc04a3d2f2928ac2224635145cb185f5555
thivinanandh <thivinanandh@gmail.com>
2024-12-27 00:12:26 +0530

    FastVPINNs: Added Transformation and Quad Routines

---
+++ src/core/sciml/FE/quad_bilinear.py
@@ -0,0 +1,183 @@
+# Copyright (c) 2024 Zenteiq Aitech Innovations Private Limited and 
+# AiREX Lab, Indian Institute of Science, Bangalore.
+# All rights reserved.
+#
+# This file is part of SciREX
+# (Scientific Research and Engineering eXcellence Platform).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any clarifications or special considerations,
+# please contact: contact@scirex.org
+"""
+The file `quad_bilinear.py` defines the Quad Bilinear transformation of the reference element.
+The implementation is referenced from the ParMooN project (File: QuadBilineare.C).
+
+Author: Thivin Anandh D
+
+Changelog: 30/Aug/2023 - Initial version
+
+Known issues: Second derivative Calculations are not implemented as of now. 
+
+Dependencies: None specified
+"""
+
+import numpy as np
+from .fe_transformation_2d import FETransforamtion2D
+
+
+class QuadBilinear(FETransforamtion2D):
+    """
+    Defines the Quad Bilinear transformation of the reference element.
+
+    :param co_ordinates: The coordinates of the reference element.
+    :type co_ordinates: numpy.ndarray
+    """
+
+    def __init__(self, co_ordinates) -> None:
+        """
+        Constructor for the QuadBilinear class.
+
+        :param co_ordinates: The coordinates of the reference element.
+        :type co_ordinates: numpy.ndarray
+        """
+        self.co_ordinates = co_ordinates
+        self.set_cell()
+        self.detjk = None  # Jacobian of the transformation
+
+    def set_cell(self):
+        """
+        Set the cell coordinates, which will be used as intermediate values to calculate the Jacobian and actual values.
+
+        :param None:
+        :type None:
+
+        :returns: None
+        :rtype: None
+        """
+        self.x0 = self.co_ordinates[0][0]
+        self.x1 = self.co_ordinates[1][0]
+        self.x2 = self.co_ordinates[2][0]
+        self.x3 = self.co_ordinates[3][0]
+
+        # get the y-coordinates of the cell
+        self.y0 = self.co_ordinates[0][1]
+        self.y1 = self.co_ordinates[1][1]
+        self.y2 = self.co_ordinates[2][1]
+        self.y3 = self.co_ordinates[3][1]
+
+        self.xc0 = (self.x0 + self.x1 + self.x2 + self.x3) * 0.25
+        self.xc1 = (-self.x0 + self.x1 + self.x2 - self.x3) * 0.25
+        self.xc2 = (-self.x0 - self.x1 + self.x2 + self.x3) * 0.25
+        self.xc3 = (self.x0 - self.x1 + self.x2 - self.x3) * 0.25
+
+        self.yc0 = (self.y0 + self.y1 + self.y2 + self.y3) * 0.25
+        self.yc1 = (-self.y0 + self.y1 + self.y2 - self.y3) * 0.25
+        self.yc2 = (-self.y0 - self.y1 + self.y2 + self.y3) * 0.25
+        self.yc3 = (self.y0 - self.y1 + self.y2 - self.y3) * 0.25
+
+    def get_original_from_ref(self, xi, eta):
+        """
+        This method returns the original coordinates from the reference coordinates.
+
+        :param xi: The xi coordinate in the reference element.
+        :type xi: float
+        :param eta: The eta coordinate in the reference element.
+        :type eta: float
+
+        :returns: The original coordinates [x, y] corresponding to the given reference coordinates.
+        :rtype: numpy.ndarray
+        """
+        x = self.xc0 + self.xc1 * xi + self.xc2 * eta + self.xc3 * xi * eta
+        y = self.yc0 + self.yc1 * xi + self.yc2 * eta + self.yc3 * xi * eta
+
+        return np.array([x, y], dtype=np.float64)
+
+    def get_jacobian(self, xi, eta):
+        """
+        This method returns the Jacobian of the transformation.
+
+        :param xi: The xi coordinate in the reference element.
+        :type xi: float
+        :param eta: The eta coordinate in the reference element.
+        :type eta: float
+
+        :returns: The Jacobian of the transformation at the given reference coordinates.
+        :rtype: float
+        """
+        self.detjk = abs(
+            (self.xc1 + self.xc3 * eta) * (self.yc2 + self.yc3 * xi)
+            - (self.xc2 + self.xc3 * xi) * (self.yc1 + self.yc3 * eta)
+        )
+        return self.detjk
+
+    def get_orig_from_ref_derivative(self, ref_gradx, ref_grady, xi, eta):
+        """
+        This method returns the derivatives of the original coordinates with respect to the reference coordinates.
+
+        :param ref_gradx: The gradient of the xi coordinate in the reference element.
+        :type ref_gradx: numpy.ndarray
+        :param ref_grady: The gradient of the eta coordinate in the reference element.
+        :type ref_grady: numpy.ndarray
+        :param xi: The xi coordinate in the reference element.
+        :type xi: float
+        :param eta: The eta coordinate in the reference element.
+        :type eta: float
+
+        :returns: The derivatives of the original coordinates [x, y] with respect to the reference coordinates.
+        :rtype: numpy.ndarray
+        """
+        n_test = ref_gradx.shape[0]
+        gradx_orig = np.zeros(ref_gradx.shape, dtype=np.float64)
+        grady_orig = np.zeros(ref_grady.shape, dtype=np.float64)
+
+        for j in range(n_test):
+            Xi = xi
+            Eta = eta
+            rec_detjk = 1 / (
+                (self.xc1 + self.xc3 * Eta) * (self.yc2 + self.yc3 * Xi)
+                - (self.xc2 + self.xc3 * Xi) * (self.yc1 + self.yc3 * Eta)
+            )
+            gradx_orig[j] = (
+                (self.yc2 + self.yc3 * Xi) * ref_gradx[j]
+                - (self.yc1 + self.yc3 * Eta) * ref_grady[j]
+            ) * rec_detjk
+            grady_orig[j] = (
+                -(self.xc2 + self.xc3 * Xi) * ref_gradx[j]
+                + (self.xc1 + self.xc3 * Eta) * ref_grady[j]
+            ) * rec_detjk
+
+        return gradx_orig, grady_orig
+
+    def get_orig_from_ref_second_derivative(
+        self, grad_xx_ref, grad_xy_ref, grad_yy_ref, xi, eta
+    ):
+        """
+        This method returns the second derivatives of the original coordinates with respect to the reference coordinates.
+
+        :param grad_xx_ref: The second derivative of the xi coordinate in the reference element.
+        :type grad_xx_ref: numpy.ndarray
+        :param grad_xy_ref: The mixed second derivative of the xi and eta coordinates in the reference element.
+        :type grad_xy_ref: numpy.ndarray
+        :param grad_yy_ref: The second derivative of the eta coordinate in the reference element.
+        :type grad_yy_ref: numpy.ndarray
+        :param xi: The xi coordinate in the reference element.
+        :type xi: float
+        :param eta: The eta coordinate in the reference element.
+        :type eta: float
+
+        :returns: The second derivatives of the original coordinates [xx, xy, yy] with respect to the reference coordinates.
+        :rtype: numpy.ndarray
+        """
+        # print(" Error : Second Derivative not implemented -- Ignore this error, if second derivative is not required ")
+        return grad_xx_ref, grad_xy_ref, grad_yy_ref
---
+++ src/core/sciml/FE/quadratureformulas_quad2d.py
@@ -0,0 +1,201 @@
+# Copyright (c) 2024 Zenteiq Aitech Innovations Private Limited and 
+# AiREX Lab, Indian Institute of Science, Bangalore.
+# All rights reserved.
+#
+# This file is part of SciREX
+# (Scientific Research and Engineering eXcellence Platform).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any clarifications or special considerations,
+# please contact: contact@scirex.org
+"""
+The file `quadratureformulas_quad2d.py` defines the Quadrature Formulas for the 2D Quadrilateral elements.
+It supports both Gauss-Legendre and Gauss-Jacobi quadrature types.
+The quadrature points and weights are calculated based on the specified quadrature order and type.
+
+Author: Thivin Anandh D
+
+Changelog: Not specified
+
+Known issues: None
+
+Dependencies: numpy, scipy
+"""
+
+import numpy as np
+from scipy.special import roots_legendre, roots_jacobi, jacobi, gamma
+from scipy.special import legendre
+from scipy.special import eval_legendre, legendre
+
+from .quadratureformulas import Quadratureformulas
+
+
+class Quadratureformulas_Quad2D(Quadratureformulas):
+    """
+    Defines the Quadrature Formulas for the 2D Quadrilateral elements.
+
+    :param quad_order: The order of the quadrature.
+    :type quad_order: int
+    :param quad_type: The type of the quadrature.
+    :type quad_type: str
+    """
+
+    def __init__(self, quad_order: int, quad_type: str):
+        """
+        Constructor for the Quadratureformulas_Quad2D class.
+
+        :param quad_order: The order of the quadrature.
+        :type quad_order: int
+        :param quad_type: The type of the quadrature.
+        :type quad_type: str
+        """
+        # initialize the super class
+        super().__init__(
+            quad_order=quad_order,
+            quad_type=quad_type,
+            num_quad_points=quad_order * quad_order,
+        )
+
+        # Calculate the Gauss-Legendre quadrature points and weights for 1D
+        # nodes_1d, weights_1d = roots_jacobi(self.quad_order, 1, 1)
+
+        quad_type = self.quad_type
+
+        if quad_type == "gauss-legendre":
+            # Commented out by THIVIN -  to Just use legendre quadrature points as it is
+            # if quad_order == 2:
+            #     nodes_1d = np.array([-1, 1])
+            #     weights_1d = np.array([1, 1])
+            # else:
+            nodes_1d, weights_1d = np.polynomial.legendre.leggauss(
+                quad_order
+            )  # Interior points
+            # nodes_1d = np.concatenate(([-1, 1], nodes_1d))
+            # weights_1d = np.concatenate(([1, 1], weights_1d))
+
+            # Generate the tensor outer product of the nodes
+            xi_quad, eta_quad = np.meshgrid(nodes_1d, nodes_1d)
+            xi_quad = xi_quad.flatten()
+            eta_quad = eta_quad.flatten()
+
+            # Multiply the weights accordingly for 2D
+            quad_weights = (weights_1d[:, np.newaxis] * weights_1d).flatten()
+
+            # Assign the values
+            self.xi_quad = xi_quad
+            self.eta_quad = eta_quad
+            self.quad_weights = quad_weights
+
+        elif quad_type == "gauss-jacobi":
+
+            def GaussJacobiWeights(Q: int, a, b):
+                [X, W] = roots_jacobi(Q, a, b)
+                return [X, W]
+
+            def jacobi_wrapper(n, a, b, x):
+
+                x = np.array(x, dtype=np.float64)
+
+                return jacobi(n, a, b)(x)
+
+            # Weight coefficients
+            def GaussLobattoJacobiWeights(Q: int, a, b):
+                W = []
+                X = roots_jacobi(Q - 2, a + 1, b + 1)[0]
+                if a == 0 and b == 0:
+                    W = 2 / ((Q - 1) * (Q) * (jacobi_wrapper(Q - 1, 0, 0, X) ** 2))
+                    Wl = 2 / ((Q - 1) * (Q) * (jacobi_wrapper(Q - 1, 0, 0, -1) ** 2))
+                    Wr = 2 / ((Q - 1) * (Q) * (jacobi_wrapper(Q - 1, 0, 0, 1) ** 2))
+                else:
+                    W = (
+                        2 ** (a + b + 1)
+                        * gamma(a + Q)
+                        * gamma(b + Q)
+                        / (
+                            (Q - 1)
+                            * gamma(Q)
+                            * gamma(a + b + Q + 1)
+                            * (jacobi_wrapper(Q - 1, a, b, X) ** 2)
+                        )
+                    )
+                    Wl = (
+                        (b + 1)
+                        * 2 ** (a + b + 1)
+                        * gamma(a + Q)
+                        * gamma(b + Q)
+                        / (
+                            (Q - 1)
+                            * gamma(Q)
+                            * gamma(a + b + Q + 1)
+                            * (jacobi_wrapper(Q - 1, a, b, -1) ** 2)
+                        )
+                    )
+                    Wr = (
+                        (a + 1)
+                        * 2 ** (a + b + 1)
+                        * gamma(a + Q)
+                        * gamma(b + Q)
+                        / (
+                            (Q - 1)
+                            * gamma(Q)
+                            * gamma(a + b + Q + 1)
+                            * (jacobi_wrapper(Q - 1, a, b, 1) ** 2)
+                        )
+                    )
+                W = np.append(W, Wr)
+                W = np.append(Wl, W)
+                X = np.append(X, 1)
+                X = np.append(-1, X)
+                return [X, W]
+
+            # get quadrature points and weights in 1D
+            x, w = GaussLobattoJacobiWeights(self.quad_order, 0, 0)
+
+            # Generate the tensor outer product of the nodes
+            xi_quad, eta_quad = np.meshgrid(x, x)
+            xi_quad = xi_quad.flatten()
+            eta_quad = eta_quad.flatten()
+
+            # Multiply the weights accordingly for 2D
+            quad_weights = (w[:, np.newaxis] * w).flatten()
+
+            # Assign the values
+            self.xi_quad = xi_quad
+            self.eta_quad = eta_quad
+            self.quad_weights = quad_weights
+
+        else:
+            print("Supported quadrature types are: gauss-legendre, gauss-jacobi")
+            print(
+                f"Invalid quadrature type {quad_type} in {self.__class__.__name__} from {__name__}."
+            )
+            raise ValueError("Quadrature type not supported.")
+
+    def get_quad_values(self):
+        """
+        Returns the quadrature weights, xi and eta values.
+
+        :return: A tuple containing the quadrature weights, xi values, and eta values.
+        :rtype: tuple
+        """
+        return self.quad_weights, self.xi_quad, self.eta_quad
+
+    def get_num_quad_points(self):
+        """
+        Returns the number of quadrature points.
+
+        :return: The number of quadrature points.
+        :rtype: int
+        """
+        return self.num_quad_points
