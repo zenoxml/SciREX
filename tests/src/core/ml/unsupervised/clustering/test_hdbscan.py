4400e466d38db376b0e48c17712b8e82d17b6c7b
Naren Vohra <narenvohra@iisc.ac.in>
2024-12-28 21:55:19 +0530

    test: HDBSCAN unit test implemented

--- tests/src/core/ml/unsupervised/clustering/test_hdbscan.py
+++ tests/src/core/ml/unsupervised/clustering/test_hdbscan.py
@@ -1,62 +1,53 @@
-# Standard library imports
-import os
-from sklearn.preprocessing import StandardScaler
+# Copyright (c) 2024 Zenteiq Aitech Innovations Private Limited and
+# AiREX Lab, Indian Institute of Science, Bangalore.
+# All rights reserved.
+#
+# This file is part of SciREX
+# (Scientific Research and Engineering eXcellence Platform).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any clarifications or special considerations,
+# please contact: contact@scirex.org
+# Author : Naren Vohra
+# Added test to check HDBSCAN clustering algorithm on benchmark dataset. 
+# The dataset is taken from "Thrun, Ultsch, 2020, Clustering benchmark
+# datasets exploiting the fundamental clustering problems, Data in Brief". 
 
-import matplotlib.pyplot as plt
-
+import pytest
 import numpy as np
-
+from sklearn.preprocessing import StandardScaler
 from scirex.core.ml.unsupervised.clustering.hdbscan import Hdbscan
 
 def test_hdbscan():
     min_samples = 5
     eps_val = 0.5
 
-    # load and scale the data
+    # Load and scale the data
     data = np.loadtxt("../../../../../support_files/chainlink_data.txt") 
     
     scaler = StandardScaler()
     data = scaler.fit_transform(data)
 
-    # perform HDBSCAN clustering
+    # Perform HDBSCAN clustering
     hdbscan = Hdbscan()
     hdbscan.fit(data)
 
-    # get number of clusters
+    # Get number of clusters
     n_clusters = hdbscan.n_clusters
     
-    # assert that number of clusters is 2
+    # Assert that number of clusters is 2
     assert (n_clusters == 2)
-
-
-    
-    # visualize the clustering data
-    labels = hdbscan.labels
-
-    unique_labels = set(labels)
-    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-    
-    plt.figure(figsize = (8, 6))
-
-    for k, col in zip(unique_labels, colors):
-        if k == -1:
-            col = [0, 0, 0, 0]
-
-        class_member_mask = labels == k
-
-        xy = data[class_member_mask]
-
-        plt.plot(xy[:, 0], xy[:, 1], "o", markerfacecolor = tuple(col), markeredgecolor = "k", markersize = 12, label = k)
-
-    plt.title("HDBSCAN clustering", fontsize = 18)
-    plt.xlabel("x", fontsize = 18)
-    plt.ylabel("y", fontsize = 18)
-    plt.xticks(fontsize = 16)
-    plt.yticks(fontsize = 16)
-    plt.legend(loc = "upper right", fontsize = 14)
-    plt.show()
-
 
 if __name__ == "__main__":
     test_hdbscan()
-
