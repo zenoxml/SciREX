d5bf6dd6dd811cb05ea847be8be6db9f82c967af
Nithya <nithyaravi1426@gmail.com>
2025-01-06 13:36:29 +0530

    Style: Added copyright

--- scirex/core/model_compression/quantization.py
+++ scirex/core/model_compression/quantization.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2024 Zenteiq Aitech Innovations Private Limited and
-# AiREX Lab, Indian Institute of Science, Bangalore.
+# Copyright (c) 2024 Zenteiq Aitech Innovations Private Limited and AiREX Lab,
+# Indian Institute of Science, Bangalore.
 # All rights reserved.
 #
 # This file is part of SciREX
@@ -7,28 +7,27 @@
 # developed jointly by Zenteiq Aitech Innovations and AiREX Lab
 # under the guidance of Prof. Sashikumaar Ganesan.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SciREX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# SciREX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# You should have received a copy of the GNU Affero General Public License
+# along with SciREX. If not, see <https://www.gnu.org/licenses/>.
 #
 # For any clarifications or special considerations,
-# please contact: contact@scirex.org
+# please contact <scirex@zenteiq.ai>
 
-
-# Author: Nithyashree R
+# Author: Nithyashree Ravikumar
 
 # This code defines a Python class QuantizationAwareTraining to enable quantization-aware training (QAT) and post-training quantization for TensorFlow models.
 # It includes methods for model evaluation, TensorFlow Lite conversion, and size measurement of both quantized and post-quantized models.
 
-import tempfile
 import os
 import numpy as np
 import tensorflow as tf
@@ -44,38 +43,43 @@ class QuantizationAwareTraining:
         q_aware_model (tf.keras.Model): The quantization-aware trained model.
     """
 
-    def __init__(self, input_shape, num_classes):
+    def __init__(
+        self, input_shape, num_classes, filters=12, kernel_size=(3, 3), pool_size=(2, 2)
+    ):
         """
         Initializes the model architecture.
 
-        :param input_shape: Shape of input data
-        :type input_shape: tuple
+        :param input_shape: Shape of input data.
         :param num_classes: Number of output classes.
-        :type num_classes: int
+        :param filters: Number of filters for the Conv2D layer.
+        :param kernel_size: Kernel size for the Conv2D layer.
+        :param pool_size: Pool size for the MaxPooling2D layer.
         """
-        self.model = self._build_model(input_shape, num_classes)
+        self.model = self._build_model(
+            input_shape, num_classes, filters, kernel_size, pool_size
+        )
         self.q_aware_model = None
 
     @staticmethod
-    def _build_model(input_shape, num_classes):
+    def _build_model(input_shape, num_classes, filters, kernel_size, pool_size):
         """
         Builds the base model architecture.
 
         :param input_shape: Shape of input data.
-        :type input_shape: tuple
         :param num_classes: Number of output classes.
-        :type num_classes: int
+        :param filters: Number of filters for the Conv2D layer.
+        :param kernel_size: Kernel size for the Conv2D layer.
+        :param pool_size: Pool size for the MaxPooling2D layer.
         :return: A Keras model.
-        :rtype: tf.keras.Model
         """
         model = tf.keras.Sequential(
             [
                 tf.keras.layers.InputLayer(input_shape=input_shape),
                 tf.keras.layers.Reshape(target_shape=input_shape + (1,)),
                 tf.keras.layers.Conv2D(
-                    filters=12, kernel_size=(3, 3), activation="relu"
+                    filters=filters, kernel_size=kernel_size, activation="relu"
                 ),
-                tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+                tf.keras.layers.MaxPooling2D(pool_size=pool_size),
                 tf.keras.layers.Flatten(),
                 tf.keras.layers.Dense(num_classes),
             ]
@@ -87,13 +91,9 @@ class QuantizationAwareTraining:
         Trains the base model.
 
         :param train_data: Training dataset.
-        :type train_data: np.ndarray
         :param train_labels: Training labels.
-        :type train_labels: np.ndarray
         :param epochs: Number of training epochs.
-        :type epochs: int
-        :param validation_split: Fraction of training data to be used for validation.
-        :type validation_split: float
+        :param validation_split: Fraction of training data for validation.
         """
         self.model.compile(
             optimizer="adam",
@@ -101,7 +101,15 @@ class QuantizationAwareTraining:
             metrics=["accuracy"],
         )
         self.model.fit(
-            train_data, train_labels, epochs=epochs, validation_split=validation_split
+            train_data,
+            train_labels,
+            epochs=epochs,
+            validation_split=validation_split,
+            callbacks=[
+                tf.keras.callbacks.EarlyStopping(
+                    monitor="val_loss", patience=3, restore_best_weights=True
+                )
+            ],
         )
 
     def apply_quantization_aware_training(self):
@@ -124,22 +132,27 @@ class QuantizationAwareTraining:
         Trains the quantization-aware model.
 
         :param train_data: Training dataset.
-        :type train_data: np.ndarray
         :param train_labels: Training labels.
-        :type train_labels: np.ndarray
         :param batch_size: Batch size for training.
-        :type batch_size: int
         :param epochs: Number of training epochs.
-        :type epochs: int
-        :param validation_split: Fraction of training data to be used for validation.
-        :type validation_split: float
+        :param validation_split: Fraction of training data for validation.
         """
+        if self.q_aware_model is None:
+            raise ValueError(
+                "Quantization-aware model is not initialized. Call `apply_quantization_aware_training` first."
+            )
+
         self.q_aware_model.fit(
             train_data,
             train_labels,
             batch_size=batch_size,
             epochs=epochs,
             validation_split=validation_split,
+            callbacks=[
+                tf.keras.callbacks.EarlyStopping(
+                    monitor="val_loss", patience=3, restore_best_weights=True
+                )
+            ],
         )
 
     def evaluate(self, test_data, test_labels):
@@ -147,11 +160,8 @@ class QuantizationAwareTraining:
         Evaluates both the base model and the quantized model.
 
         :param test_data: Test dataset.
-        :type test_data: np.ndarray
         :param test_labels: Test labels.
-        :type test_labels: np.ndarray
         :return: Accuracy of base model and quantized model.
-        :rtype: tuple
         """
         baseline_accuracy = self.model.evaluate(test_data, test_labels, verbose=0)[1]
         q_aware_accuracy = self.q_aware_model.evaluate(
@@ -164,7 +174,6 @@ class QuantizationAwareTraining:
         Converts the quantization-aware model to TensorFlow Lite format.
 
         :return: Quantized TFLite model.
-        :rtype: bytes
         """
         converter = tf.lite.TFLiteConverter.from_keras_model(self.q_aware_model)
         converter.optimizations = [tf.lite.Optimize.DEFAULT]
@@ -175,7 +184,6 @@ class QuantizationAwareTraining:
         Applies post-training quantization to the base model.
 
         :return: Post-quantized TFLite model.
-        :rtype: bytes
         """
         converter = tf.lite.TFLiteConverter.from_keras_model(self.model)
         converter.optimizations = [tf.lite.Optimize.DEFAULT]
@@ -187,12 +195,10 @@ class QuantizationAwareTraining:
         Saves the TFLite model to a file.
 
         :param model_content: The TFLite model content.
-        :type model_content: bytes
         :param filename: File name to save the model.
-        :type filename: str
         """
-        with open(filename, "wb") as f:
-            f.write(model_content)
+        with open(filename, "wb") as file:
+            file.write(model_content)
 
     @staticmethod
     def measure_model_size(filepath):
@@ -200,8 +206,6 @@ class QuantizationAwareTraining:
         Measures the size of a model file.
 
         :param filepath: Path to the model file.
-        :type filepath: str
         :return: Size of the model in megabytes.
-        :rtype: float
         """
         return os.path.getsize(filepath) / float(2**20)
