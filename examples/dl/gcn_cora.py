f0787582d9d51adbbeca715f751ff8e907d348a3
Rajarshi <rajarshid@iisc.ac.in>
2025-01-11 10:22:39 +0530

    GCN: Added GCN module with example and test

--- examples/dl/gcn_cora.py
+++ examples/dl/gcn_cora.py
@@ -1,40 +1,90 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Copyright (c) 2024 Zenteiq Aitech Innovations Private Limited and
+# AiREX Lab, Indian Institute of Science, Bangalore.
+# All rights reserved.
+#
+# This file is part of SciREX
+# (Scientific Research and Engineering eXcellence Platform),
+# developed jointly by Zenteiq Aitech Innovations and AiREX Lab
+# under the guidance of Prof. Sashikumaar Ganesan.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any clarifications or special considerations,
+# please contact: contact@scirex.org
 
-# # Graph Convolution Network (GCN) for a classification problem
-# 
-# We will work with the CORA dataset from pytorch geometirc.
-# Each paper is assingned a high dimensional vector based on its contents
-# and we will work with the directed graph formed by the citations.
-# Each paper or node is classified based on research topic.
+"""
+    Example Script: gcn_cora.py
 
-# In[1]:
+    This script demonstrates how to use the graph convolution network
+    implementation from the SciREX library to perform classification
+    on the CORA dataset from pytorch geometirc.
 
+    This example includes:
+        - Loading the CORA dataset using pytorch.geometric
+        - Training Graph Convolutional Neural Networks
+        - Evaluating the results
+
+    Key Features:
+        - Uses cross-entropy loss for training
+        - Implements accuracy metric for evaluation
+
+    Authors:
+        - Rajarshi Dasgupta (rajarshid@iisc.ac.in)
+
+    Version Info:
+        - 10/01/2025: Initial version
+
+"""
+
+import jax
+import jax.numpy as jnp
+
+from scirex.core.dl.gcn import GCN, GCNModel
 
 from torch_geometric.datasets import Planetoid
 
 # Load the CORA dataset
-dataset = Planetoid(root='/tmp/Cora', name='Cora')
+data = Planetoid(root='/tmp/Cora', name='Cora')[0]
 
-# Access the first graph object
-data = dataset[0]
-
-
-# In[2]:
-
+"""
+    From the CORA dataset from pytorch geometric we obtain
+        - node feature vectors
+            There are 2,708 nodes.
+            Each node represents a scientific publication.
+            The size of the node feature vector is 1,433
+            and has values 0 or 1.
+            A dictionary of 1,433 unique words
+            is used to assign the node feature vector.
+            The i-th value of the node feature vector is 1,
+            if the i-th word in the dictionary is present
+            in the research paper represented by the node and 0 otherwise.
+        - node labels
+            There are 7 classes indicating the topics of the research paper
+            and the nodes have labels 0 to 6.
+        - edges
+            Research publication contain citations
+            to other scientific works.
+            The dataset provides edges
+            which are a set of pairs of nodes connected by citation.
+            The citation network is represented by a directed graph.
+"""
 
 print("No. of nodes = ", data['x'].shape[0])
 print("No. of edges = ", data['edge_index'].shape[1])
 print("Classification labels range from ", data['y'].min(), " to ", data['y'].max())
 
 
-# To work with the jax ecosystem we will change the data type to a jax array
-
-# In[3]:
-
-
-import jax.numpy as jnp
-
+# To work with the jax ecosystem we change the data type to a jax ndarray
 x = jnp.asarray(data['x'].numpy())
 edge_index = jnp.asarray(data['edge_index'].numpy())
 y = jnp.asarray(data['y'].numpy())
@@ -44,113 +94,84 @@ node_vector_size = x.shape[1]
 num_classes = int(y.max()) + 1
 
 
-# We will use the edge index information to form the adjacency matrix which will be 2D jax array. Ideally we should use sparse matrices for efficiency.
-
-# In[4]:
-
-
+# Adjacency matrix and degree array are formed
 A = jnp.zeros((num_nodes,num_nodes))
 A = A.at[edge_index[0], edge_index[1]].set(1)
-
-
 degree = A.sum(axis=0) # Check axis
 
 print("Adjacency matrix of dimensions ", A.shape)
 print("Min degree ", degree.min())
 print("Max degree ", degree.max())
 
-
-# Now we will use the gcn class defined in gcn.py to transform the node features to vectors of size = number of classes. Initially the parameters of the transformation are set randomly.
-
-# In[5]:
-
-
-import jax
-from scirex.core.dl.gcn import GCN, GCNModel
-
+# The GCN class is used to transform the input for classification
 key = jax.random.PRNGKey(42)
-
 model_key, key = jax.random.split(key)
-gcn_transformation = GCN([node_vector_size, 100, 100, num_classes], model_key)
 
+gcn = GCN(
+        [node_vector_size, 100, 100, num_classes],
+        [jnp.tanh]*3,
+        model_key
+        )
 
-# We will randomly select nodes for training. We will use one-hot encodings as target vectors.
-
-# In[6]:
-
-
+# One-hot encodings are generated for the training set
 num_of_nodes_in_training_set = int(0.5 * num_nodes)
-training_set = jax.random.choice(key, num_nodes, (num_of_nodes_in_training_set,), replace=False)
+training_set = jax.random.choice(
+        key,
+        num_nodes,
+        (num_of_nodes_in_training_set,),
+        replace=False
+        )
 
 Y = jnp.zeros((num_nodes, num_classes), dtype=int)
 Y = Y.at[training_set,y[training_set]].set(1)
 
 
-# Now we can define our cross entropy loss function for the training set. To define the model, we apply the softmax function to the node embeddings we get as output. This is useful as we will generate one-hot encodings as our target output and use the cross-entropy function.
-
-# In[7]:
-
-
+# The loss function is evaluated by the cross entropy loss on the gcn output
 def loss_fn(output, target):
     predicted_probs =  jax.nn.softmax(output, axis = 1)
     cross_entropy_terms = - target * jnp.log(predicted_probs)
     return cross_entropy_terms.sum()
 
 
-# We define functions for calculating the training and overall accuracy
-
-# In[8]:
-
-
+# Functions for calculating the training and overall accuracy are defined
 def accuracy_fn(labels, node_indices):
     def subset_accuracy(output):
         predicted_probs =  jax.nn.softmax(output, axis = 1)[node_indices]
         predicted_labels = predicted_probs.argmax(axis = 1)
-        predicted_correct = jnp.where(labels[node_indices] == predicted_labels, 1, 0)
+        predicted_correct = jnp.where(
+                labels[node_indices] == predicted_labels,
+                1,
+                0)
         return predicted_correct.sum() / predicted_correct.shape[0]
     return subset_accuracy
 
 training_accuracy = accuracy_fn(y, training_set)
 overall_accuracy = accuracy_fn(y, jnp.arange(num_nodes))
 
-model = GCNModel(gcn_transformation, loss_fn, [training_accuracy, overall_accuracy])
+# The model is defined and trained
 
-# Now we begin the training process
+model = GCNModel(
+        gcn,
+        loss_fn,
+        [training_accuracy, overall_accuracy]
+        )
 
-# In[9]:
+gcn = model.fit(
+        x,
+        A,
+        degree,
+        Y,
+        learning_rate = 5e-2,
+        num_iters = 50)
 
-gcn_transformation = model.fit(x, A, degree, Y, learning_rate = 5e-2, num_iters = 50)
-
-# # Now let us plot the loss plot
-
-# import matplotlib.pyplot as plt
-# 
-# plt.plot(loss_values)
-# plt.xlabel("optim iteration")
-# plt.ylabel("loss value")
-# plt.grid()
-# plt.show()
-# 
-# 
-# # In[11]:
-# 
-# 
-# plt.plot(train_acc_list, label="train")
-# plt.plot(overall_acc_list, label="overall")
-# plt.legend()
-# plt.xlabel("optim iteration")
-# plt.ylabel("accuracy")
-# plt.grid()
-# plt.show()
-# 
-# 
-# # In[14]:
-# 
-# 
-
-final_node_embeddings = gcn_transformation(x, A, degree)
-final_training_accuracy_percentage = training_accuracy(final_node_embeddings) * 100
-final_overall_accuracy_percentage = overall_accuracy(final_node_embeddings) * 100
+# Results
+final_node_embeddings = gcn(x, A, degree)
+final_training_accuracy_percentage = 100.0 * training_accuracy(
+        final_node_embeddings
+        )
+final_overall_accuracy_percentage = 100.0 * overall_accuracy(
+        final_node_embeddings
+        )
 
 print(f"Final training accuracy = {final_training_accuracy_percentage:.2f}%")
 print(f"Final overall accuracy = {final_overall_accuracy_percentage:.2f}%")
--- scirex/core/dl/gcn.py
+++ scirex/core/dl/gcn.py
@@ -1,3 +1,47 @@
+# Copyright (c) 2024 Zenteiq Aitech Innovations Private Limited and
+# AiREX Lab, Indian Institute of Science, Bangalore.
+# All rights reserved.
+#
+# This file is part of SciREX
+# (Scientific Research and Engineering eXcellence Platform),
+# developed jointly by Zenteiq Aitech Innovations and AiREX Lab
+# under the guidance of Prof. Sashikumaar Ganesan.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any clarifications or special considerations,
+# please contact: contact@scirex.org
+
+"""
+    Module: gcn.py
+
+    This module implements Graph Convolution Networks (GCNs)
+
+    It provides functionality to:
+        - Perform transformations on Graphs using GCN
+        - Train GCNs for a loss function
+
+    Key Features:
+        - Built on top of base class getting all its functionalities
+        - Efficient neural networks implementation using equinox modules
+
+    Authors:
+        - Rajarshi Dasgupta (rajarshid@iisc.ac.in)
+
+    Version Info:
+        - 10/01/2025: Initial version
+
+"""
 import jax
 import jax.numpy as jnp
 
@@ -11,24 +55,23 @@ class GCN(eqx.Module):
     W_list: list
     B_list: list
 
-    # activations: list[callable]
+    activations: list
 
-    def __init__(
-            self,
-            layers,
-            # activations,
-            key):
+    def __init__(self, layers, activations, key):
         """
+        Initialize a GCN instance with random initial parameters
+
         Inputs:
-            layers is a python list indicating the size of the node embeddings at each layer
-            key is used to generate random numbers for initialising the W and B matrices
+            layers: a python list indicating the size of the node embeddings at each layer
+            activations: a python list of activation functions
+            key: to generate random numbers for initialising the W and B matrices
         """
         
         self.num_layers = len(layers)
         self.W_list = []
         self.B_list = []
 
-        # self.activations = activations
+        self.activations = activations
 
         for i in range(self.num_layers-1):
             weights_key, bias_key, key = jax.random.split(key, num=3)
@@ -40,18 +83,20 @@ class GCN(eqx.Module):
 
     def __call__(self, z, adj_mat, degree):
         """
-        Inputs:
-            z is a jnp array for which the i-th row is the i-th node embedding
-            adj_mat is the adjacency matrix. Ideally it should be a sparse matrix
-            degree is a jnp array where the i-th element is the degree of the i-th node
+        Initialize the gcn model with network architecture and training parameters.
+
+        Args:
+            z: jnp array for which the i-th row is the i-th node embedding
+            adj_mat: the adjacency matrix. Ideally it should be a sparse matrix
+            degree: jnp array where the i-th element is the degree of the i-th node
 
         Output:
-            Similar to z. The node embeddings of the output
+            node embeddings of the output
         """
 
-        activation = jnp.tanh
-        # for activation,W,B in zip(self.activations,self.W_list,self.B_list):
-        for W,B in zip(self.W_list,self.B_list):
+        # activation = jnp.tanh
+            # for W,B in zip(self.W_list,self.B_list):
+        for activation,W,B in zip(self.activations,self.W_list,self.B_list):
             z = activation(jnp.diagflat(1.0/degree) @ adj_mat @ z @ W + z @ B)
         return z
 
@@ -59,14 +104,23 @@ class GCNModel():
 
     def __init__(
         self,
-        gcn_transformation: GCN,
+        gcn: GCN,
         loss_fn: callable,
         metrics: list[callable] = [],
         ):
+        """
+        Initialize the gcn model with network architecture and training parameters.
 
-        self.gcn_transformation = gcn_transformation
+        Args:
+            gcn: Neural network architecture to train.
+            loss_fn (Callable): Loss function for training.
+            metrics (list[Callable]): List of metric functions for evaluation.
+        """
+
+        self.gcn = gcn
         self.loss_fn = loss_fn
         self.metrics = metrics
+
 
     def fit(
         self,
@@ -86,37 +140,82 @@ class GCNModel():
             adjacency_matrix: jnp.ndarray,
             degree_array: jnp.ndarray,
             target: jnp.ndarray,
-            learning_rate: float,
-            num_iters: int = 10,
-            num_check_points: int = 5
+            learning_rate: Parameter of the gradient based optimisation method
+            num_iters: Number of iterations of the gradient based optimisation method
+            num_check_points
+
+        Returns:
+            Trained gcn
         """
+        gcn = self.gcn
+
+        self.optimizer = optax.adam(learning_rate)
+        opt_state = self.optimizer.init(eqx.filter(gcn, eqx.is_array))
+
         check_point_gap = num_iters / num_check_points
 
-        optim = optax.adam(learning_rate = learning_rate)
-        opt_state = optim.init(self.gcn_transformation)
-
         for iter_id in tqdm(range(num_iters), desc="Training", total=num_iters):
-            loss, grads = eqx.filter_value_and_grad(self._loss_fn)(
-                    self.gcn_transformation,
+
+            loss, gcn, opt_state = self._update_step(
+                    gcn,
                     features,
                     adjacency_matrix,
                     degree_array,
-                    target
-                    )
-
-            updates, opt_state = optim.update(grads, opt_state)
-            self.gcn_transformation = eqx.apply_updates(self.gcn_transformation, updates)
+                    target,
+                    opt_state
+                    ) 
 
             if iter_id % check_point_gap == 0:
-                output = self.gcn_transformation(features, adjacency_matrix, degree_array)
+                output = gcn(features, adjacency_matrix, degree_array)
                 metric_vals = [ m(output) for m in self.metrics ]
                 print(f"Iter: {iter_id} | Loss: {loss:.2e} | Metrics {metric_vals}")
 
-        return self.gcn_transformation
+        return gcn
+
+    @eqx.filter_jit
+    def _update_step(
+            self,
+            gcn: GCN,
+            features: jnp.ndarray,
+            adjacency_matrix: jnp.ndarray,
+            degree_array: jnp.ndarray,
+            target: jnp.ndarray,
+            opt_state
+            ):
+        """
+        Perform single training step with JIT compilation.
+
+        Args:
+            gcn: GCN,
+            features: Input feature vectors
+            adjacency_matrix
+            degree_array
+            target
+            opt_state
+
+        Returns:
+            tuple: Tuple containing:
+                - Average loss for the batch
+                - Updated network
+                - Updated optimizer state
+        """
+        loss, grads = eqx.filter_value_and_grad(self._loss_fn)(
+                gcn,
+                features,
+                adjacency_matrix,
+                degree_array,
+                target
+                )
+
+        updates, opt_state = self.optimizer.update(grads, opt_state, eqx.filter(gcn, eqx.is_array)
+)
+        gcn = eqx.apply_updates(gcn, updates)
+
+        return loss, gcn, opt_state
 
     def _loss_fn(
             self,
-            gcn_transformation: GCN,
+            gcn: GCN,
             features: jnp.ndarray,
             adjacency_matrix,
             degree_array,
@@ -127,15 +226,13 @@ class GCNModel():
         Required for getting gradients during training and JIT.
 
         Args:
-            net (Network): Neural network to compute loss.
-            x (jnp.ndarray): Input features for loss computation.
-            y (jnp.ndarray): Target values for loss computation.
+            gcn: Instance of GCN
+            features: Input feature vectors
+            adjacency_matrix,
+            degree_array,
+            target: jnp.ndarray
 
         Returns:
             jnp.ndarray: Loss value.
         """
-        # Check why the vmap does not work properly
-        # return jax.vmap(self.loss_fn)(jax.vmap(gcn_transformation)(features, adjacency_matrix, degree_array), target).mean()
-        return self.loss_fn(gcn_transformation(features, adjacency_matrix, degree_array), target).mean()
-
-
+        return self.loss_fn(gcn(features, adjacency_matrix, degree_array), target).mean()
